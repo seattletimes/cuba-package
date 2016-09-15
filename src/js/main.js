2ce672b466b83a8b55925e4c99f3af43f1f80eb7
Audrey Carlsen <acarlsen@seattletimes.com>
2016-09-15 11:24:57 -0700

    Gallery works and lazy loads

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,24 @@ require("./lib/social");
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+
+var qsa = require("./lib/qsa.js");
+
+qsa(".gallery").forEach(function(g) {
+  g.addEventListener("click", function(e) {
+    var target = e.target;
+    var current = g.querySelector(".active");
+    if (target.classList.contains("next")) {
+      var next = current.nextElementSibling;
+      if (next) var nextNext = next.nextElementSibling;
+    } else if (target.classList.contains("prev")){
+      var next = current.previousElementSibling;
+      if (next) var nextNext = next.previousElementSibling;
+    }
+    if (!next || !next.classList.contains("gallery-img")) return;
+    if (!next.src) next.src = next.getAttribute("data-src");
+    if (nextNext && !nextNext.src) nextNext.src = nextNext.getAttribute("data-src");
+    current.classList.remove("active");
+    next.classList.add("active");
+  });
+});
