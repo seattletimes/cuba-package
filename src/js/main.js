82ec142638e8f7138f76d1da733b54271fb46352
Audrey Carlsen <acarlsen@seattletimes.com>
2016-09-16 14:35:30 -0700

    angel text updates

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,12 @@ require("component-responsive-frame/child");
 var qsa = require("./lib/qsa.js");
 
 qsa(".gallery").forEach(function(g) {
+  g.querySelector(".caption").innerHTML = g.querySelector(".active img").alt;
+  var nextImg = g.querySelector(".active + .gallery-img img");
+  nextImg.src = nextImg.getAttribute("data-src");
   g.addEventListener("click", function(e) {
     var target = e.target;
+    var caption = g.querySelector(".caption");
     var current = g.querySelector(".active");
     if (target.classList.contains("next")) {
       var next = current.nextElementSibling;
@@ -18,8 +22,13 @@ qsa(".gallery").forEach(function(g) {
       if (next) var nextNext = next.previousElementSibling;
     }
     if (!next || !next.classList.contains("gallery-img")) return;
-    if (!next.src) next.querySelector("img").src = next.querySelector("img").getAttribute("data-src");
-    if (nextNext && nextNext.classList.contains("gallery-img") && !nextNext.src) nextNext.querySelector("img").src = nextNext.querySelector("img").getAttribute("data-src");
+    var image = next.querySelector("img");
+    caption.innerHTML = image.alt;
+    if (!image.src) image.src = image.getAttribute("data-src");
+    if (nextNext && nextNext.classList.contains("gallery-img")) {
+      var nnImage = nextNext.querySelector("img");
+      if (!nnImage.src) nnImage.src = nnImage.getAttribute("data-src");
+    }
     current.classList.remove("active");
     next.classList.add("active");
   });
