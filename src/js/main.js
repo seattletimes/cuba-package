3e5bc2acc919db3d035b5c12ed32778f5f17e5a8
Audrey Carlsen <acarlsen@seattletimes.com>
2016-09-22 15:11:53 -0700

    adds recirculation

--- src/js/main.js
+++ src/js/main.js
@@ -115,12 +115,22 @@ window.addEventListener("scroll", function() {
 
 $(".sidestory").forEach(function(r) {
   r.addEventListener("click", function(e) {
-    if (e.target.classList.contains("read-more")) {
+    if (e.target.classList.contains("read-more") || e.target.classList.contains("icon") || e.target.classList.contains("fa")) {
+      var inner = r.querySelector(".inner");
       if (r.classList.contains("expanded")) {
-        r.classList.remove("expanded");
+        var height = inner.offsetHeight;
+        inner.style.height = height + "px";
+        inner.classList.add("animate");
+        var reflow = inner.offsetHeight;
+        inner.style.height = "0px";
+        setTimeout(function() {
+          inner.classList.remove("animate");
+          inner.style.height = "";
+          r.classList.remove("expanded");
+        }, 300);
+        
       } else {
         r.classList.add("expanded");
-        var inner = r.querySelector(".inner");
         var height = inner.offsetHeight;
         inner.style.height = "0px";
         inner.classList.add("animate");
